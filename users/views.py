5886d708db5c832941096b9612d8d660a55cf469
unniznd <aanandunnis@gmail.com>
2023-03-05 15:05:28 +0530

    updated users

--- users/views.py
+++ users/views.py
@@ -83,10 +83,10 @@ class ShopOwnerPublicView(ListAPIView):
 class CustomAuthToken(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
-        request._mutable = True
+        # request._mutable = True
         
-        request.data['password'] = fernet.decrypt((request.data['password']).encode()).decode()
-        request._mutable = False
+        # request.data['password'] = fernet.decrypt((request.data['password']).encode()).decode()
+        # request._mutable = False
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
